9af75f90f1d965831979a0705f641a1424739ad8
tlMark <127731453+tlMark@users.noreply.github.com>
2025-05-30 01:28:44 +0900

    구매 및 저장 시스템 구축

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Sprite[] jellySpriteList;
+    public string[] jellyNameList;
+    public int[] jellyJelatineList;
     public int[] jellyGoldList;
 
     public Vector3[] PointList;
-
     public RuntimeAnimatorController[] LevelAc;
 
     public void ChangeAc(Animator anim, int level)
---
+++ Assets/Script/Jelly.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Jelly
+{
+    public int ID;
+    public int Level;
+    public float Exp;
+
+    public Jelly(int ID, int Level, float Exp)
+    {
+        this.ID = ID;
+        this.Level = Level;
+        this.Exp = Exp;
+    }
+}
